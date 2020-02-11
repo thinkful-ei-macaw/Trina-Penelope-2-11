ca05e078bd4dc3a348cbbbae08903dff9ebdc045
Trina <trinamceary@gmail.com>
2020-02-11 10:25:39 -0800

    while loop outline begin attempt

---
+++ drills.js
@@ -0,0 +1,15 @@
+'use strict';
+const numList = [-5, 28, 98, -20013, 0.7878, 22, 115];
+
+max (numbers){
+  let i = 0;
+  while (numbers[i] <= numbers.length){
+    i++;
+  }
+}
+
+min (numbers) {
+  while (){
+    
+  }
+}
