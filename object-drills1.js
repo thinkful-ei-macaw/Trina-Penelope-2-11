64602bcf740341f8c1e44d384017f43008efe9bc
Trina <trinamceary@gmail.com>
2020-02-11 20:17:32 -0800

    complete createMyObjectFunction

---
+++ object-drills1.js
@@ -0,0 +1,13 @@
+'use strict';
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello;';
+    }
+
+  };
+} 
+console.log(createMyObject());
